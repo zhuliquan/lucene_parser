6239f59b6e680f53e523914bf581da99f2f786fa
zhuliquan <zlq164114@sina.com>
2022-04-26 23:51:58 +0800

    fix: remove useless string func

--- operator/const.go
+++ operator/const.go
@@ -9,17 +9,6 @@ const (
 	MUST_NOT_PREFIX_TYPE
 )
 
-var prefixOPType_Values = map[PrefixOPType]string{
-	UNKNOWN_PREFIX_TYPE:  "",
-	SHOULD_PREFIX_TYPE:   " ",
-	MUST_PREFIX_TYPE:     " +",
-	MUST_NOT_PREFIX_TYPE: " -",
-}
-
-func (o PrefixOPType) String() string {
-	return prefixOPType_Values[o]
-}
-
 type LogicOPType uint32
 
 const (
@@ -28,14 +17,3 @@ const (
 	OR_LOGIC_TYPE
 	NOT_LOGIC_TYPE
 )
-
-var LogicOPType_Values = map[LogicOPType]string{
-	UNKNOWN_LOGIC_TYPE: "",
-	AND_LOGIC_TYPE:     " AND ",
-	OR_LOGIC_TYPE:      " OR ",
-	NOT_LOGIC_TYPE:     "NOT ",
-}
-
-func (o LogicOPType) String() string {
-	return LogicOPType_Values[o]
-}
